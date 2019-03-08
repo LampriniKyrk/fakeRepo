5f38719aef96a404ced607ee0a31b9c00ff7e7bd
Nana-Comquent <lakyrkou@csd.auth.gr>
2019-03-08 16:39:45 +0200

    inserted a new class named as MyClass

--- src/com/fake/repo/Main.java
+++ src/com/fake/repo/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, jim.");
+
+
+        MyClass myClass = new MyClass(10);
+
     }
 
     public void exampleForPrinting() {
---
+++ src/com/fake/repo/MyClass.java
@@ -0,0 +1,10 @@
+package com.fake.repo;
+
+public class MyClass {
+
+
+    public MyClass(int x) {
+        System.out.println("The number you gave me is: " + x);
+    }
+
+}
